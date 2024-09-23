f59ee85e0c653ac8e79c2effa7deb1f4b43256ec
raccoonRyu <rjhwind94@gmail.com>
2024-09-23 13:54:50 +0900

    변수의 효용 예제 작성

---
+++ src/CH02_Variable/VariableUtil.java
@@ -0,0 +1,15 @@
+package CH02_Variable;
+
+public class VariableUtil {
+    public static void main(String[] args) {
+        // 변수의 효용
+        // 변수를 선언하는 것은 값에 이름을 주는 것
+        // 이러한 이름을 통해 특정 값의 수정이 필요할 때 반복되는 작업을 줄일 수 있다.
+        // 또한 코드를 보는 사람이 코드의 의미를 빨리 파악할 수 있도록 한다.
+        String name = "JH";
+        System.out.println("Hello, "+name+" ... "+name+" ... bye");
+
+        double VAT = 10.0;
+        System.out.println(VAT);
+    }
+}
